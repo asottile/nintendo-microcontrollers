35d360d12b675dc0c9b484446d99442e8557ed7b
Anthony Sottile <asottile@umich.edu>
2023-01-05 10:21:56 -0500

    change serial_debug to a monitor with timing

--- serial_debug.py
+++ serial_debug.py
@@ -2,11 +2,17 @@ from __future__ import annotations
 
 import argparse
 import os.path
+import select
 import sys
+import time
 
 import serial
 
 SERIAL_DEFAULT = 'COM1' if sys.platform == 'win32' else '/dev/ttyUSB0'
+
+
+def t() -> bytes:
+    return f'[{time.time():.3f}]'.encode()
 
 
 def main() -> int:
@@ -14,21 +20,25 @@ def main() -> int:
     parser.add_argument('--serial', default=SERIAL_DEFAULT)
     args = parser.parse_args()
 
+    nl = True
     with serial.Serial(args.serial, 9600) as ser:
         ser.write(b'V')
-        while True:
-            if os.path.exists('f'):
-                with open('f', 'rb') as f:
-                    contents = f.read()
-                os.remove('f')
-                ser.write(contents)
-                sys.stdout.buffer.write(b'> send: ')
-                sys.stdout.buffer.write(contents)
-                sys.stdout.buffer.write(b'\n')
-                sys.stdout.buffer.flush()
-            elif ser.in_waiting:
-                sys.stdout.buffer.write(ser.read())
-                sys.stdout.buffer.flush()
+        try:
+            rlist = (ser.fd,)
+            while True:
+                select.select(rlist, (), ())
+                if ser.in_waiting:
+                    if nl:
+                        sys.stdout.buffer.write(t())
+                    read = os.read(ser.fd, 100)
+                    nl = read.endswith(b'\n')
+                    read = read.rstrip(b'\n')
+                    sys.stdout.buffer.write(read.replace(b'\n', b'\n' + t()))
+                    if nl:
+                        sys.stdout.buffer.write(b'\n')
+                    sys.stdout.buffer.flush()
+        finally:
+            ser.write(b'v')
     return 0
 
 
