a38928a38a3434b6fae50fb2b9542dff9091d294
Anthony Sottile <asottile@umich.edu>
2023-01-28 14:41:33 -0500

    add script for resetting outbreaks

---
+++ scripts/sv/reset_outbreaks.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import argparse
+
+import cv2
+import serial
+
+from scripts.engine import do
+from scripts.engine import match_text
+from scripts.engine import Point
+from scripts.engine import Press
+from scripts.engine import run
+from scripts.engine import SERIAL_DEFAULT
+from scripts.engine import States
+from scripts.engine import Wait
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--serial', default=SERIAL_DEFAULT)
+    args = parser.parse_args()
+
+    vid = cv2.VideoCapture(0)
+    vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    def check(frame: object) -> bool:
+        try:
+            input('is this the right raid? (enter to skip, ^D to exit)')
+        except (EOFError, KeyboardInterrupt):
+            print('\nbye!')
+            raise SystemExit(0)
+        else:
+            return True
+
+    states: States = {
+        'INITIAL': (
+            (
+                match_text(
+                    'Map',
+                    Point(y=89, x=224),
+                    Point(y=125, x=280),
+                    invert=False,
+                ),
+                do(
+                    Press('H'), Wait(1),
+                    Press('s'),
+                    Press('d', duration=.55),
+                    Press('A'), Wait(1),
+                    Press('s', duration=1.3),
+                    Press('A'), Wait(.75),
+                    Press('s', duration=.7),
+                    Press('A'), Wait(.75),
+                    Press('s'), Press('s'),
+                    Press('A'), Wait(.75),
+                    Press('d'), Press('w'),
+                    Press('d', duration=.6), Wait(.2), Press('A'), Wait(.75),
+                    Press('H'), Wait(1), Press('H'), Wait(4),
+                ),
+                'CONFIRM',
+            ),
+        ),
+        'CONFIRM': ((check, do(), 'INITIAL'),),
+    }
+
+    with serial.Serial(args.serial, 9600) as ser:
+        run(vid=vid, ser=ser, initial='INITIAL', states=states)
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
